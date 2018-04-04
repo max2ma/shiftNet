85f5eb3ce60189c4edb15318ad140a28895e429e
math@shaheen <liang.ma@studenti.polito.it>
2018-04-04 21:18:57 +0200

    basic functions with stream interface

--- shift.h
+++ shift.h
@@ -1,58 +1,108 @@
 #pragma once
 
-#include "hls_half.h"
-template<typename T, int Df, int M, int N>
-void _shift(T fmap[Df][Df][M], T act[Df][Df][N],
-		const int Dx[M],
-		const int Dy[M],
-		const T p[M][N]){
-#pragma HLS INLINE
+#include "hls_stream.h"
+#include "buffer.h"
 
-	_shift_loopI:for(int i=0;i<Df;i++){
-		_shift_loopJ:for(int j=0;j<Df;j++){
-			_shift_loopN:for(int n=0;n<N;n++){
-				half sum = 0;
-				_shift_loopM:for(int m=0;m<M;m++){
-					int dx = i - Dx[m];
-					int dy = j - Dy[m];
-					if(dx<0 || dx>=Df || dy<0 || dy>=Df)
-						continue;
-					sum += p[m][n].to_half() * fmap[dx][dy][m].to_half();
-				}
-				act[i][j][n] = T(sum);
+template<typename T, int D, int C>
+void _relu(hls::stream<T>& fmap, hls::stream<T>& act){
+	for(int i=0;i<D;i++){
+		for(int j=0;j<D;j++){
+			for(int k=0;k<C;k++){
+#pragma HLS PIPELINE
+				T diff = fmap.read();
+				if(diff < 0)
+					act.write(0);
+				else
+					act.write(diff);
 			}
 		}
 	}
 }
 
-template<typename T, int Df, int M, int N>
-void _shift_n(T fmap[Df][Df][M], T act[Df][Df][N],
-		const int Dx[M],
-		const int Dy[M],
-		const T p[M][N]){
-#pragma HLS INLINE
-#pragma HLS ARRAY_PARTITION variable=p complete dim=1
+template<typename T, int D, int C, int S>
+void _shift_3x3(hls::stream<T>& fmap, hls::stream<T>& act,
+		const int Dx[C],
+		const int Dy[C]){
+	static const int nD = (D - 1)/S + 1;
+	ShiftBuffer<T,2,D,C> buffer;
+	for(int i=0;i<D;i++)
+		for(int j=0;j<C;j++){
+#pragma HLS PIPELINE
+			T r = fmap.read();
+			buffer.insert(r, j);
+		}
+	for(int i=0;i<D;i++){
+		for(int j=0;j<D;j++){
+			for(int k=0;k<C;k++){
+#pragma HLS PIPELINE
+				T r,w;
+				if(i != D -1)
+					r = fmap.read();
+				else
+					r = 0;
+
+				switch(Dy[k]){
+					case 0:
+						switch(Dx[k]){
+							case 0:
+								buffer.ih[k] = 0;
+								w = buffer.insert(r,k);
+								break;
+							case 1:
+								w = buffer.insert(r,k);
+								break;
+							case -1:
+								w = r;
+//								buffer.insert(r,k);
+								break;
+						}
+						break;
+					case 1:
+						if(j ==0) {
+							buffer.ih[k] = 0;
+							buffer.pixel[k].shift_pixels_right();
+							buffer.pixel[k].insert_pixel(0, 0, 0);
+						}
+						w = buffer.insert(r,k);
+						break;
+					case -1:
+						if(j ==0) {
+							buffer.ih[k] = 0;
+							buffer.pixel[k].shift_pixels_left();
+							buffer.pixel[k].insert_pixel(0, 0, D - 1);
+						}
+						w = buffer.insert(r,k);
+						break;
+				}
+				if(i % S == 0 && j % S ==0)
+					act.write(w);
+			}
+		}
+	}
+}
+
+template<typename T,
+int D, int C, int N>
+void _linear_combination(hls::stream<T> &fmap, hls::stream<T> &out, const T p[C][N]){
+
 	T sum[N];
 #pragma HLS ARRAY_PARTITION variable=sum complete dim=1
+
 	for(int i=0;i<N;i++)
+#pragma HLS PIPELINE
 		sum[i] = 0;
 
-	for(int i=0;i<Df;i++){
-		for(int j=0;j<Df;j++){
-			for(int m=0;m<M;m++){
+	for(int i=0;i<D;i++){
+		for(int j=0;j<D;j++){
+			for(int k=0;k<C;k++){
 #pragma HLS PIPELINE
-				int dx = i - Dx[m];
-				int dy = j - Dy[m];
-				if(dx<0 || dx>=Df || dy<0 || dy>=Df)
-					continue;
-				for(int n=0;n<N;n++){
-					sum[n] += p[m][n] * fmap[dx][dy][m];
-				}
-
+				T tmp = fmap.read();
+				for(int n=0;n<N;n++)
+					sum[n] += p[k][n] * tmp;
 			}
 			for(int n=0;n<N;n++){
 #pragma HLS PIPELINE
-				act[i][j][n] = sum[n];
+				out.write(sum[n]);
 				sum[n] = 0;
 			}
 		}
@@ -60,71 +110,25 @@ void _shift_n(T fmap[Df][Df][M], T act[Df][Df][N],
 }
 
 
-template<typename T, int Df, int M, int N, int S>
-void _shift_s(T fmap[Df][Df][M], T act[(Df - 1)/S + 1][(Df - 1)/S + 1][N],
-		const int Dx[M],
-		const int Dy[M],
-		const T p[M][N]){
-#pragma HLS ARRAY_PARTITION variable=p complete dim=2
-	T sum[N];
-#pragma HLS ARRAY_PARTITION variable=sum complete dim=1
-	for(int i=0;i<N;i++)
-		sum[i] = 0;
+template<typename T, int D, int C, int S>
+void _max_pool(hls::stream<T> &fmap, hls::stream<T> &act){
 
-	static const int nD = (Df - 1)/S + 1;
-
-	for(int i=0;i<nD;i++){
-		for(int j=0;j<nD;j++){
-			for(int m=0;m<M;m++){
+	static const int nD = D/S;
+	T buffer[nD][C];
+	int c =0, is =0, js =0;
+	for(int i = 0, is = 0;i<D;i++, is++)
+		for(int j = 0, js = 0, c = 0;j<D;j++, js++)
+			for(int k = 0;k<C;k++){
 #pragma HLS PIPELINE
-				int dx = i * S - Dx[m];
-				int dy = j * S - Dy[m];
-				if(dx<0 || dx>=Df || dy<0 || dy>=Df)
-					continue;
-				for(int n=0;n<N;n++){
-					sum[n] += p[m][n] * fmap[dx][dy][m];
+				T r = fmap.read();
+				if(is == S) is = 0;
+				if(js == S) {
+					js = 0;
+					c++;
 				}
+				if((is == 0 && js ==0) || buffer[c][k] < r)
+					buffer[c][k] = r;
+				if( is == S - 1 && js == S -1)
+					act.write(buffer[c][k]);
 			}
-			for(int n=0;n<N;n++){
-#pragma HLS PIPELINE
-				act[i][j][n] = sum[n];
-				sum[n] = 0;
-			}
-		}
-	}
-}
-
-template<typename T, int Df, int M, int N, int S>
-void _shift_r(T fmap[Df][Df][M], T act[(Df - 1)/S + 1][(Df - 1)/S + 1][N],
-		const int Dx[M],
-		const int Dy[M],
-		const T p[M][N]){
-#pragma HLS ARRAY_PARTITION variable=p complete dim=1
-	T sum[N];
-#pragma HLS ARRAY_PARTITION variable=sum complete dim=1
-	for(int i=0;i<N;i++)
-		sum[i] = 0;
-
-	static const int nD = (Df - 1)/S + 1;
-
-	for(int i=0;i<nD;i++){
-		for(int j=0;j<nD;j++){
-			for(int m=0;m<M;m++){
-#pragma HLS PIPELINE
-				int dx = i * S - Dx[m];
-				int dy = j * S - Dy[m];
-				if(dx<0 || dx>=Df || dy<0 || dy>=Df)
-					continue;
-				for(int n=0;n<N;n++){
-					sum[n] += p[m][n] * fmap[dx][dy][m];
-				}
-
-			}
-			for(int n=0;n<N;n++){
-#pragma HLS PIPELINE
-				act[i][j][n] = sum[n] > 0? sum[n] : 0;
-				sum[n] = 0;
-			}
-		}
-	}
 }
