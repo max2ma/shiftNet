8e6ab8871c7fe888481fe299fac73bbc4dcd3355
math@shaheen <liang.ma@studenti.polito.it>
2018-04-07 01:57:11 +0200

    unify the name for the parameters

--- shift.cpp
+++ shift.cpp
@@ -1,8 +1,8 @@
+#include <cstring>
 #include "hls_stream.h"
 #include "para.h"
 #include "shift.h"
 using namespace para;
-
 
 void shift(hls::stream<DataType> & tensor,
 		hls::stream<DataType> & act){
@@ -10,21 +10,18 @@ void shift(hls::stream<DataType> & tensor,
 #pragma HLS INTERFACE axis register both port=tensor
 #pragma HLS INTERFACE axis register both port=act
 
-		const int Dx[M]={
-#include "dx"
+		const int D0[C * E]={
+#include "d_0"
 		};
-		const int Dy[M]={
-#include "dy"
+		const DataType p0[C][C * E]={
+#include "p0_0"
 		};
-		const DataType p0[C][M]={
-#include "p0"
-		};
-		const DataType p1[M][N]={
-#include "p1"
+		const DataType p1[C * E][N]={
+#include "p1_0"
 		};
 
 #pragma HLS DATAFLOW
-		_shift<DataType, D, sS, cS, mS, C, M, N>(tensor, act, Dx, Dy,p0,p1);
+		_shift<DataType, D, sS, cS, C, E, N>(tensor, act, D0 ,p0,p1);
 }
 
 
