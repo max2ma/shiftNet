4e5c1e25b8e783777446fc32256a3188ec3764fd
math@shaheen <liang.ma@studenti.polito.it>
2018-04-11 01:37:06 +0200

    add macros to switch between several implementation

--- main.cpp
+++ main.cpp
@@ -7,47 +7,64 @@ using namespace para;
 
 extern
 void shift(hls::stream<DataType> *tensor, hls::stream<DataType>* act);
+extern
+void shift(hls::stream<DataType> &tensor, hls::stream<DataType> & act);
 
 int main(){
 	DataType input[D][D][C] = {
 #include "input"
 	};
 
-	float ref[nD][nD][N] = {
-#include "t_act"
+	float ref[N] = {
+#include "t_cifar"
 	};
 
+	DataType out[N];
 
+#ifdef SINGLE_NET
+hls::stream<DataType> istream, ostream;
+#elif defined MUL_NET
 hls::stream<DataType> istream[C], ostream[N];
+#endif
+
 for(int i=0;i<D;i++)
 		for(int j=0;j<D;j++)
 			for(int k=0;k<C;k++)
+#ifdef SINGLE_NET
+				istream.write(input[i][j][k]);
+#elif defined MUL_NET
 				istream[k].write(input[i][j][k]);
-
+#endif
 
 	shift(istream, ostream);
 
 
 	int err = 0;
 	float ave = 0;
-	for(int i=0;i<nD;i++)
-		for(int j=0;j<nD;j++)
+//	for(int i=0;i<nD;i++)
+//		for(int j=0;j<nD;j++)
 			for(int k=0;k<N;k++){
+#ifdef SINGLE_NET
+				DataType output = ostream.read();
+#elif defined MUL_NET
 				DataType output = ostream[k].read();
+#endif
 #ifdef FIXED
-				float diff = abs(( output - (DataType)ref[i][j][k]).to_float());
+				float diff = abs(( output - (DataType)ref[k]).to_float());
 				ave+=diff;
 #else
-				float diff = abs(output - ref[i][j][k]);
+				float diff = abs(output - ref[k]);// ref[i][j][k]);
 				ave+=diff;
 #endif
 				if (diff > 1e-2){
 					err ++;
-					cout <<i<<','
-							<<j<<','
-							<<k<<','
+					//cout <<i<<','
+					//		<<j<<','
+					cout	<<k<<','
 							<<output<<','
-							<<ref[i][j][k]<<endl;
+							//<<ref[i][j][k]
+							<<ref[k]
+							<<endl;
 
 				}
 			}
--- para.h
+++ para.h
@@ -3,21 +3,22 @@
 #include "hls_half.h"
 
 
-#define FIXED
+#define MUL_NET
+//#define FIXED
 
 #ifdef FIXED
 typedef ap_fixed<16, 4> DataType;
 #else
-typedef float DataType;
+typedef half DataType;
 #endif
 namespace para{
-	const int D = 11;
+	const int D = 32;
 	const int C = 3;
-	const int E = 2;
+	const int E = 1;
 	const int M = E*C;
-	const int N = 16;
+	const int N = 10;
 	const int sS = 1;
-	const int cS = 2;
+	const int cS = 1;
 	const int mS = 2;
 	const int nD = (D - 1)/sS/cS + 1;
 }
