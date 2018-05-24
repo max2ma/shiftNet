ac5629d65b8d4962d834377da8082ad37c85013d
math@shaheen <liang.ma@studenti.polito.it>
2018-05-24 18:40:07 +0200

    intputs, weights and output can have different data type now

--- shift.h
+++ shift.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include "utils/x_hls_traits.h"
 #include "hls_stream.h"
+#include "log2.h"
 
 namespace MulChan{
 
@@ -99,7 +101,8 @@ namespace MulChan{
 #pragma HLS ARRAY_PARTITION variable=out complete dim=1
 #pragma HLS ARRAY_PARTITION variable=fmap complete dim=1
 			static const int nD = D/S;
-			T buffer[nD][C];
+			typedef typename hls::x_traits<T, ap_uint<CE_LOG2<S*S>::V> >::MULT_T SUM_T;
+			SUM_T buffer[nD][C];
 #pragma HLS ARRAY_PARTITION variable=buffer complete dim=2
 			for(int rep = 0; rep < REP; rep++){
 				int col =0, is =0, js =0;
@@ -128,8 +131,8 @@ namespace MulChan{
 		}
 
 
-	template< int D, int C, int IIs, int REP, typename T>
-		void _bias_add(hls::stream<T> fmap[C], const T bias[C], hls::stream<T> out[C]){
+	template< int D, int C, int IIs, int REP, typename T1, typename T2, typename T3>
+		void _bias_add(hls::stream<T1> fmap[C], const T2 bias[C], hls::stream<T3> out[C]){
 #pragma HLS INLINE
 #pragma HLS ARRAY_PARTITION variable=fmap complete dim=1
 #pragma HLS ARRAY_PARTITION variable=bias complete dim=1
@@ -144,8 +147,8 @@ namespace MulChan{
 					}
 				}
 		}
-	template< int D, int C, int IIs, int REP, typename T>
-		void _add(hls::stream<T> fmap_0[C], hls::stream<T> fmap_1[C], hls::stream<T> out[C]){
+	template< int D, int C, int IIs, int REP, typename T1, typename T2, typename T3>
+		void _add(hls::stream<T1> fmap_0[C], hls::stream<T2> fmap_1[C], hls::stream<T3> out[C]){
 #pragma HLS INLINE
 #pragma HLS ARRAY_PARTITION variable=fmap_0 complete dim=1
 #pragma HLS ARRAY_PARTITION variable=fmap_1 complete dim=1
@@ -201,14 +204,17 @@ namespace MulChan{
 			}
 		}
 
-	template< int D, int C, int N, int S, int IIs, int REP, typename T>
-		void _conv2d_1x1(hls::stream<T> fmap[C], hls::stream<T> out[N], const T p[C][N]){
+	template< int D, int C, int N, int S, int IIs, int REP, typename T_IN, typename T_W, typename T_OUT>
+		void _conv2d_1x1(hls::stream<T_IN> fmap[C], hls::stream<T_OUT> out[N], const T_W p[C][N]){
 #pragma HLS INLINE
 
 #pragma HLS ARRAY_PARTITION variable=p complete dim=0
 #pragma HLS ARRAY_PARTITION variable=fmap complete dim=1
 #pragma HLS ARRAY_PARTITION variable=out complete dim=1
-			T sum[N], tmp[C];
+			typedef typename hls::x_traits<T_IN, T_W>::MULT_T MULT_T;
+			typedef typename hls::x_traits<MULT_T, ap_uint<CE_LOG2<C>::V> >::MULT_T SUM_T;
+			SUM_T sum[N];
+			T_IN tmp[C];
 #pragma HLS ARRAY_PARTITION variable=sum complete dim=1
 #pragma HLS ARRAY_PARTITION variable=tmp complete dim=1
 
@@ -237,26 +243,26 @@ namespace MulChan{
 					}
 				}
 		}
-	template< int D, int C, int N, int IIs, int REP, typename T>
-		void _matMul(hls::stream<T> fmap[C], hls::stream<T> out[N], const T p[D*D*C][N]){
+	template< int M, int C, int N, int IIs, int REP, typename T_IN, typename T_W, typename T_OUT>
+		void _matMul(hls::stream<T_IN> fmap[C], hls::stream<T_OUT> out[N], const T_W p[M*C][N]){
 #pragma HLS INLINE
 #pragma HLS ARRAY_PARTITION variable=p complete dim=2
-			T sum[N], r[C];
+			typedef typename hls::x_traits<T_IN, T_W>::MULT_T MULT_T;
+			typedef typename hls::x_traits<MULT_T, ap_uint<CE_LOG2<C>::V> >::MULT_T SUM_T;
+			SUM_T sum[N], r[C];
 #pragma HLS ARRAY_PARTITION variable=sum complete dim=1
 #pragma HLS ARRAY_PARTITION variable=r complete dim=1
 			for(int n=0;n<N;n++)
 #pragma HLS UNROLL
 				sum[n] = 0;
 			for(int rep = 0; rep < REP; rep++){
-				for(int i=0;i<D;i++){
-					for(int j=0;j<D;j++){
+				for(int i=0;i<M;i++){
 #pragma HLS PIPELINE II=IIs
 						for(int k=0;k<C;k++){
 							r[k] = fmap[k].read();
 							for(int n=0;n<N;n++)
-								sum[n] += p[i*D*C + j*C+ k][n] * r[k];
+								sum[n] += p[i*C + k][n] * r[k];
 						}
-					}
 				}
 				for(int n=0;n<N;n++){
 #pragma HLS PIPELINE
@@ -266,22 +272,24 @@ namespace MulChan{
 			}
 		}
 
-	template<int D, int IP, int E, int IIs, int REP, typename T>
-		void _shift(hls::stream<T> input[IP],
-				hls::stream<T> output[IP],
-				const T p0[IP][IP * E],
-				const T p1[IP * E][IP],
-				const T bias0[IP*E],
-				const T bias1[IP]
+	template<int D, int IP, int E, int IIs, int REP, typename T_S,
+		typename T_IN, typename T_OUT, typename T0, typename T1, typename TB0, typename TB1>
+		void _shift(hls::stream<T_IN> input[IP],
+				hls::stream<T_OUT> output[IP],
+				const T0 p0[IP][IP * E],
+				const T1 p1[IP * E][IP],
+				const TB0 bias0[IP*E],
+				const TB1 bias1[IP]
 				){
 			static const int MP = IP * E;
 
 #pragma HLS INLINE
-			hls::stream<T> f_in0[IP], f_in1[IP];
+			hls::stream<T_IN> f_in0[IP], f_in1[IP];
 #pragma HLS STREAM VARIABLE=f_in1 DEPTH=D*2*REP
 			_duplicate<D, IP, REP>(input, f_in0, f_in1);
 
-			hls::stream<T> f_conv0[MP], f_bias0[MP], f_shift[MP],f_conv1[IP], f_bias1[IP], f_relu[MP], f_relu1[IP];
+			hls::stream<T_S> f_conv0[MP], f_bias0[MP], f_relu[MP],f_shift[MP];
+			hls::stream<T_OUT> f_conv1[IP], f_bias1[IP],  f_relu1[IP];
 
 			_conv2d_1x1<D, IP, MP, 1, IIs, REP>(f_in0, f_conv0, p0);
 			_bias_add< D, MP, IIs, REP>(f_conv0, bias0, f_bias0);
@@ -293,26 +301,27 @@ namespace MulChan{
 
 			_add< D, IP, IIs, REP>(f_relu1, f_in1, output);
 		}
-	template<int D, int S_conv, int IP, int E, int OP, int IIs, int REP, typename T>
-		void _shift_res(hls::stream<T> input[IP],
-				hls::stream<T> output[OP],
-				const T p0[IP][OP * E],
-				const T p1[OP * E][OP],
-				const T p2[IP][OP],
-				const T bias0[OP*E],
-				const T bias1[OP],
-				const T bias2[OP]
+	template<int D, int S_conv, int IP, int E, int OP, int IIs, int REP, typename T_S,
+		typename T_IN, typename T_OUT, typename T0, typename T1, typename T2, typename TB0, typename TB1, typename TB2>
+		void _shift_res(hls::stream<T_IN> input[IP],
+				hls::stream<T_OUT> output[OP],
+				const T0 p0[IP][OP * E],
+				const T1 p1[OP * E][OP],
+				const T2 p2[IP][OP],
+				const TB0 bias0[OP*E],
+				const TB1 bias1[OP],
+				const TB2 bias2[OP]
 				){
 			static const int MP = OP * E;
 			static const int nD = (D - 1)/S_conv + 1;
 
 #pragma HLS INLINE
-			hls::stream<T> f_in0[IP], f_in1[IP];
+			hls::stream<T_IN> f_in0[IP], f_in1[IP];
 #pragma HLS STREAM VARIABLE=f_in1 DEPTH=D*2*REP
 			_duplicate< D, IP, REP>(input, f_in0, f_in1);
 
-			hls::stream<T> f_conv0[MP],f_bias0[MP], f_shift[MP], f_relu[MP];
-			hls::stream<T> f_conv1[OP],f_bias1[OP], f_relu1[OP], f_shortcut[OP], f_bias2[OP];
+			hls::stream<T_S> f_conv0[MP],f_bias0[MP], f_shift[MP], f_relu[MP];
+			hls::stream<T_OUT> f_conv1[OP],f_bias1[OP], f_relu1[OP], f_shortcut[OP], f_bias2[OP];
 
 			_conv2d_1x1<D, IP, MP, 1, IIs, REP>(f_in0, f_conv0, p0);
 			_bias_add< D, MP, IIs, REP>(f_conv0, bias0, f_bias0);
